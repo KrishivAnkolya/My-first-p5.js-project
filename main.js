4858793246504228787a51ea6e49988f6024f35a
KrishivAnkolya <106598568+KrishivAnkolya@users.noreply.github.com>
2023-03-23 22:21:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function preload() {
+
+} 
+
+function setup() {
+    canvas = createCanvas(640, 480); 
+    canvas.position(110, 250); 
+    video = createCapture(VIDEO); 
+    video.hide(); 
+
+    tint_color = ""; 
+} 
+
+function draw() {
+
+} 
+    
+    
+function take_snapshot(){
+    save('student_name.png'); 
+}
